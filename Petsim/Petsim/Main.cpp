b39bfaa6613dc86dfba9269078ae6499ead3483b
Harvestminer <Harvestminer@gmail.com>
2017-05-21 23:56:18 -0500

    Added to Class pets
    Added Public and Private
    set the pets hunger and fun to 0
    Added voids Talk, Eat, Play that set food and fun to 4
    Added money, addmoney, filled, enjoyment, gethunger, getfun integers
    Added Void Pass Time!

--- Petsim/Petsim/Main.cpp
+++ Petsim/Petsim/Main.cpp
@@ -4,9 +4,10 @@
 //	Started 5/21/2017
 //------------------------------------------------------------------------
 
-//	List of things we need::
+//	List of things we need:
 //	way to feed, way to play, way to buy, way to earn money, way to name,
 //	way to save, way to view our money, and way to view our pets stats.
+//	Costs $12 to buy a pet.
 //------------------------------------------------------------------------
 
 #include <iostream>
@@ -14,8 +15,25 @@
 using namespace std;
 
 class pets {
+public:
+	pets(int hunger = 0, int fun = 0);
+	void Talk();
+	void Eat(int food = 4);
+	void Play(int fun = 4);
+	int money;
+	int addmoney;
+
+private:
+	int Filled;
+	int Enjoyment;
+
+	int GetHunger() const;
+	int GetFun() const;
+	void PassTime(int time = 1);
 
 };
+
+
 
 int main() {
 
