d7d85572015d70e9d152888043848eb178549058
Harvestminer <Harvestminer@gmail.com>
2017-05-21 23:31:53 -0500

    Added a few more lines.
    Added a list of things needed.
    Added a pets class.

--- Petsim/Petsim/Main.cpp
+++ Petsim/Petsim/Main.cpp
@@ -4,9 +4,18 @@
 //	Started 5/21/2017
 //------------------------------------------------------------------------
 
+//	List of things we need::
+//	way to feed, way to play, way to buy, way to earn money, way to name,
+//	way to save, way to view our money, and way to view our pets stats.
+//------------------------------------------------------------------------
+
 #include <iostream>
 
 using namespace std;
+
+class pets {
+
+};
 
 int main() {
 
